ea09831a9946b05f515a29fc6837af5d2215a179
Kovynev-Pavel <148617295+Kovynev-Pavel@users.noreply.github.com>
2023-11-17 12:08:28 +0300

    Update interpolyation.py

--- interpolyation.py
+++ interpolyation.py
@@ -10,6 +10,13 @@ class interpolyation:
             if X[i - 1] <= iks <= X[i]:
                 igrek = (Y[i] - Y[i - 1]) / (X[i] - X[i - 1]) * (iks - X[i - 1]) + Y[i - 1]
                 return igrek
+    def count_kvadrat_nterpolyation(self, iks):
+        for i in range(1, len(X)):
+            if X[i - 1] <= iks <= X[i + 1]:
+                action1 = Y[i - 1] * ((iks - X[i]) * (iks - X[i + 1])) / ((X[i - 1] - X[i]) * (X[i - 1] - X[i + 1]))
+                action2 = Y[i] * ((iks - X[i - 1]) * (iks - X[i + 1])) / ((X[i] - X[i - 1]) * (X[i] - X[i + 1]))
+                action3 = (action1 + action2) + Y[i + 1] * ((iks - X[i - 1]) * (iks - X[i])) / ((X[i + 1] - X[i - 1]) * (X[i + 1] - X[i]))
+                print(action3)
 
 
 
