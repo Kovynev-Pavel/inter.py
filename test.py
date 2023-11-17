8cc83a4070fbdb77c6221c810ae66204eb1e48d2
Kovynev-Pavel <148617295+Kovynev-Pavel@users.noreply.github.com>
2023-11-17 12:09:17 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ from interpolyation import interpolyation
 inter1 = interpolyation()
 inter1.add_in_list({3: 1, 4: 2, 5: 4})
 inter1.count_interpolyation(3.2)
+inter1.count_kvadrat_nterpolyation(3.002)
